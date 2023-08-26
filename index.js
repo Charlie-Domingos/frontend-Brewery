32a421a3997972bc6534c37dbd6f2d2bf7b5ee95
charlie-harpe <charlie.augusto@outlook.com>
2023-08-26 12:21:01 -0300

    layout pagination button

--- index.js
+++ index.js
@@ -1,141 +1,139 @@
-const perPage = 1;
-const maxPage = 3;
-let currentPage = 1;
+const perPage = 20
+const maxPage = 3
+let currentPage = 1
 
-document.getElementById("backbutton").addEventListener("click", () => {
-  window.location.href = "/index.html";
-});
+document.getElementById('backbutton').addEventListener('click', () => {
+  window.location.href = '/index.html'
+})
 
 const fetchPage = () => {
   fetch(
     `https://api.openbrewerydb.org/v1/breweries?page=${currentPage}&per_page=${perPage}`
   )
-    .then((res) => res.json())
-    .then((data) => {
-      const container = document.getElementById("gridItem");
-      console.log(data);
+    .then(res => res.json())
+    .then(data => {
+      const container = document.getElementById('gridItem')
+      console.log(data)
 
-      data.forEach((item) => {
-        const div = document.createElement("div");
-        div.className = "brewery-item";
+      data.forEach(item => {
+        const div = document.createElement('div')
+        div.className = 'brewery-item'
 
-        const name = document.createElement("h2");
-        name.textContent = item.name;
-        div.appendChild(name);
+        const name = document.createElement('h2')
+        name.textContent = item.name
+        div.appendChild(name)
 
-        const address = document.createElement("p");
-        address.textContent = `${item.address_1} - ${item.state_province} - ${item.postal_code} ${item.country}`;
+        const address = document.createElement('p')
+        address.textContent = `${item.address_1} - ${item.state_province} - ${item.postal_code} ${item.country}`
 
-        div.appendChild(address);
+        div.appendChild(address)
 
-        let contentAdded = false;
+        let contentAdded = false
 
         // This function show more details about brewery, when you click on it
         function detailsBrewery() {
-          div.addEventListener("click", () => {
+          div.addEventListener('click', () => {
             if (!contentAdded) {
-              gridItem.innerHTML = "";
-              address.innerHTML = "";
+              gridItem.innerHTML = ''
+              address.innerHTML = ''
 
               // TYPE CONFIG
-              const type = document.createElement("p");
-              type.textContent = `Type: ${item.brewery_type}`;
-              div.appendChild(type);
+              const type = document.createElement('p')
+              type.textContent = `Type: ${item.brewery_type}`
+              div.appendChild(type)
 
               // STREET CONFIG
-              const street = document.createElement("p");
-              street.textContent = `Street: ${item.street}`;
-              div.appendChild(street);
+              const street = document.createElement('p')
+              street.textContent = `Street: ${item.street}`
+              div.appendChild(street)
 
               // CITY CONFIG
-              const city = document.createElement("p");
-              city.textContent = `City: ${item.city}`;
-              div.appendChild(city);
+              const city = document.createElement('p')
+              city.textContent = `City: ${item.city}`
+              div.appendChild(city)
 
               // STATE CONFIG
-              const state = document.createElement("p");
-              city.textContent = `State: ${item.state}`;
-              div.appendChild(state);
+              const state = document.createElement('p')
+              city.textContent = `State: ${item.state}`
+              div.appendChild(state)
 
               // POSTALCODE CONFIG
-              const postal_code = document.createElement("p");
-              postal_code.textContent = `Postal code: ${item.postal_code}`;
-              div.appendChild(postal_code);
+              const postal_code = document.createElement('p')
+              postal_code.textContent = `Postal code: ${item.postal_code}`
+              div.appendChild(postal_code)
 
               // COUNTRY CONFIG
-              const country = document.createElement("p");
-              country.textContent = `Country: ${item.country}`;
-              div.appendChild(country);
+              const country = document.createElement('p')
+              country.textContent = `Country: ${item.country}`
+              div.appendChild(country)
 
               // WEBSITE CONFIG
-              const website_url = document.createElement("a");
-              website_url.href = item.website_url;
-              website_url.target = "_blank";
+              const website_url = document.createElement('a')
+              website_url.href = item.website_url
+              website_url.target = '_blank'
 
-              const websiteText = document.createTextNode("Website: ");
-              website_url.appendChild(websiteText);
+              const websiteText = document.createTextNode('Website: ')
+              website_url.appendChild(websiteText)
 
-              website_url.textContent = `${item.website_url}`;
-              div.appendChild(websiteText);
-              div.appendChild(website_url);
+              website_url.textContent = `${item.website_url}`
+              div.appendChild(websiteText)
+              div.appendChild(website_url)
 
               // PHONE CONFIG
-              const phone = document.createElement("p");
-              phone.textContent = `Phone: ${item.phone}`;
-              div.appendChild(phone);
+              const phone = document.createElement('p')
+              phone.textContent = `Phone: ${item.phone}`
+              div.appendChild(phone)
 
               // LOCATE CONFIG
-              const locate = document.createElement("a");
-              locate.target = "_blank";
+              const locate = document.createElement('a')
+              locate.target = '_blank'
 
-              const locateText = document.createTextNode("Open in maps: ");
-              locate.appendChild(locateText);
+              const locateText = document.createTextNode('Open in maps: ')
+              locate.appendChild(locateText)
 
-              locate.href = `https://www.google.com.br/maps?q=${item.latitude},${item.longitude}`;
-              locate.textContent = `${item.latitude}, ${item.longitude}`;
-              div.appendChild(locateText);
-              div.appendChild(locate);
+              locate.href = `https://www.google.com.br/maps?q=${item.latitude},${item.longitude}`
+              locate.textContent = `${item.latitude}, ${item.longitude}`
+              div.appendChild(locateText)
+              div.appendChild(locate)
 
               // REMOVE HIDDEN backButton
-              document.getElementById("backbutton").classList.remove("hidden");
-              document.getElementById("pageNumbers").classList.add("hidden");
+              document.getElementById('backbutton').classList.remove('hidden')
+              document.getElementById('pageNumbers').classList.add('hidden')
 
-              container.appendChild(div);
-              contentAdded = true;
+              container.appendChild(div)
+              contentAdded = true
             }
-          });
+          })
         }
 
-        detailsBrewery();
+        detailsBrewery()
 
-        container.appendChild(div);
-      });
+        container.appendChild(div)
+      })
     })
-    .catch((error) => alert("Erro ao carregar dados da API:", error));
-};
-
+    .catch(error => alert('Erro ao carregar dados da API:', error))
+}
 
 // Page buttons pagination funcion
 const updatePageButtons = () => {
-  const pageNumbersDiv = document.getElementById("pageNumbers");
-  document.getElementById("pageNumbers").classList.remove("hidden")
-  pageNumbersDiv.innerHTML = ""; // clean the previuos page
+  const pageNumbersDiv = document.getElementById('pageNumbers')
+  document.getElementById('pageNumbers').classList.remove('hidden') // remove pageNumbers hidden
+  pageNumbersDiv.innerHTML = '' // clean the previuos page
 
   for (let i = 1; i <= maxPage; i++) {
-    const pageNumberButton = document.createElement("button");
-    pageNumberButton.textContent = i;
-    pageNumberButton.classList.add("pageNumberButton")
-    pageNumberButton.addEventListener("click", () => {
-      currentPage = i;
-      gridItem.innerHTML = "";
-      fetchPage();
-      updatePageButtons();
-    });
-    pageNumbersDiv.appendChild(pageNumberButton);
+    const pageNumberButton = document.createElement('button')
+    pageNumberButton.textContent = i
+    pageNumberButton.classList.add('pageNumberButton')
+    pageNumberButton.addEventListener('click', () => {
+      currentPage = i
+      gridItem.innerHTML = ''
+      fetchPage()
+      updatePageButtons()
+    })
+    pageNumbersDiv.appendChild(pageNumberButton)
   }
-};
+}
 
 // Load first page
-fetchPage();
-updatePageButtons();
-
+fetchPage()
+updatePageButtons()
